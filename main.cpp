4079ab41c574ced5c221edc713a6721984dbd970
Çağrı Uluç Yıldırımoğlu <cagriuluc@sabanciuniv.edu>
2020-05-01 23:12:26 +0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 using namespace std;
 
 
-
 class Graph
 {
 public:
@@ -67,12 +66,16 @@ private:
 			return edge;
 		}
 		else{
-			return min(edge, edge1 + edge2)
+			return min(edge, edge1 + edge2);
 		}
 
 	}	
 };
 
+void fillEdgesBoth(string filename,Graph & g1, Graph & g2)
+{
+	readEdges(filename, g1.edges, g2.edges);
+}
 
 
 class GraphMapper
@@ -266,6 +269,8 @@ int main()
 
 	Graph g1(numberOfVertices);
 	Graph g2(numberOfVertices);
+	
+	fillEdgesBoth("graphs/50v_9.txt",g1,g2);
 
 	GraphMapper graphMapper(g1, g2);
 
@@ -273,6 +278,7 @@ int main()
 
 	if (graphMapper.isomorphismFound) {
 		// show solution in output file by using graphMapper.vertexMap
+		cout << "Isomorphism found" << endl;
 	}
 	else {
 		cout << "No isomorphism found" << endl;
